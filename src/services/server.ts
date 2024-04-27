69c49cb31c8fb0a432567ba759832f5efd6cc0c2
mwang840 <maxwang@udel.edu>
2024-04-27 10:08:13 -0400

    doing modular imports

--- src/services/server.ts
+++ src/services/server.ts
@@ -1,94 +1,22 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import express from "express";
 
+const app = express()
+dotenv.config();
 
-const mongoose = require("mongoose")
-const Perks = require("Listings.ts")
-require("dotenv").config({path: "./config.env"})
-mongoose.set("strictQuery", false);
-mongoose.connect(String(process.env.MONGO_URI), {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+const PORT: string = process.env.PORT!
+const MONGO_URI: string = process.env.MONGO_URI!
 
-const connection = mongoose.connection;
-
-const userSchema = new mongoose.Schema({
-    emailAddress: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    userName: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-  });
-
-  const ItemSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-        unique: true
-    }
-  })
-
-  const CategorySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    value: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    emoji: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    description: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-  })
-
-  const ListingSchema = mongoose.Schema({
-    id: {
-        type: Number,
-        required: true,
-        unique: true,
-    },
-    title: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    date: {
-        type: Date,
-        required: true,
-        unique: true
-    },
-    location: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    perks:{
-        type: String,
-        required: true,
-        unique: true
-    }
-  })
+mongoose
+	.connect(MONGO_URI)
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(
+				`Successfully connected to MongoDB! Server listening on port ${PORT}`
+			);
+		});
+	})
+	.catch(err => {
+		console.log(err);
+	});
